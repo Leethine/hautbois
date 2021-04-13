c5dd6a35552d5d72adc3361fe90f631e63f6956c
Tzu-An <zian.li@mines-ales.org>
2021-04-13 18:21:06 +0200

    python template for the music score

---
+++ py_module/musicscore/Note.py
@@ -0,0 +1,19 @@
+"""
+Note.py
+
+Definition of the Note class, representing an individual musical note.
+
+Licenced under GPL 3.0
+
+"""
+
+
+class Note:
+    
+    self.__Name      = ""
+    self.__LatinName = ""
+    self.__NoteIndex = 0
+    self.__Duration  = 0
+    
+    def __init__(self):
+        pass
