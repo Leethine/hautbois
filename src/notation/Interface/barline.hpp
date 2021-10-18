4075ea7b316da3506bee91e41bf755150c052ad5
Tzu-An <zian.li.1996@outlook.com>
2021-10-18 23:38:39 +0200

    create barline interface

--- src/notation/Interface/barline.hpp
+++ src/notation/Interface/barline.hpp
@@ -1,4 +1,5 @@
 #include "../onebar.hpp"
+#include <numeric>
 #include <regex>
 
 namespace hautbois {
@@ -7,34 +8,54 @@ using BarPtrArray=std::vector<std::unique_ptr<OneBar>>;
 
 using TokenString=std::string;
 
-class OneBarFacade : private OneBar {
+class BarLine : private OneBar {
     mutable BarPtrArray barline;
-
-    bool checkBarComplete() const {}
+    
+    bool checkBarComplete() const {
+        Duration d;
+        for (auto it = begin(notes); it != end(notes); it++) {
+            d = d + (*it)->getDuration();
+        }
+        if (d == meter) { return true; }
+        else { return false; }
+    }
 
     NoteType checkType(TokenString& token) const {}
     
     bool checkRestNoteFormatting(TokenString& token) const {}
+
     bool checkSingleNoteFormatting(TokenString& token) const {}
+    
     bool checkGroupNoteFormatting(TokenString& token) const {}
 
     bool checkFormatting(NoteType type, TokenString& token) const {}
 
-    void strAddSingleNote(TokenString& name) {}
     void strAddSingleNote(TokenString& name, TokenString& duration) {}
+
     void strAddSingleNote(TokenString& name, TokenString& duration,
                        TokenString& property) {}
+    
     void strAddSingleNote(TokenString& name, TokenString& duration,
                        TokenString& property, TokenString& ornament) {}
+    
     void strAddRestNote(TokenString& duration) {}
     
-    void strAddGroupNote(TokenString& token) {}
+    void strAddGroupNote(GroupName& names, GroupDuration& durations) {}
+    
+    void strAddGroupNote(GroupName& names, GroupDuration& durations, 
+                         TokenString& property) {}
 
+    void tokenAddRestNote(TokenString& token) {}
+    
+    void tokenAddSingleNote(TokenString& token) {}
+    
+    void tokenAddGroupNote(TokenString& token) {}
+    
     public:
-    OneBarFacade() : OneBar ()
+    BarLine() : OneBar ()
     {}
     
-    explicit OneBarFacade (NotationSystemName& name, 
+    explicit BarLine (NotationSystemName& name, 
     NoteName& scale, Beat num, Beat denom) : 
     OneBar(name, scale, num, denom) 
     {}
---
+++ test/test_barinterface.cpp
@@ -0,0 +1,17 @@
+#include "../src/notation/Interface/barline.hpp"
+#include <iostream>
+
+using namespace hautbois; 
+using namespace std;
+
+int main() {
+    BarLine b1;
+    
+    /*
+    b1.appendRestNote({1,4});
+    b1.appendSingleNote("C4", {1,4});
+    b1.appendGroupNote({"C4", "E4", "G4"}, {{1,4},{1,4},{1,4}});
+    b1.appendGroupNote({"D4", "F#4", "A4"}, {1,4});
+    cout << boolalpha << b1.checkBarComplete() << endl;
+    */
+}
