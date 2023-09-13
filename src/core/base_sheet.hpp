f12a713dc1c597af924a58d77b3ec2579c3876fe
Z L <zian.li.1996@outlook.com>
2023-09-13 23:11:22 +0200

    Add BasicSheet class

--- src/core/base_sheet.hpp
+++ src/core/base_sheet.hpp
@@ -12,9 +12,41 @@ namespace core {
 class BaseSheet {
  private:
   std::string _title;
+  std::string _subtitle;
+  std::string _subsubtitle;
+  std::string _composer;
+
+  std::string _opus;
+  std::string _opus_system;
+  UInt32 _opus_number;
+  std::string _work_type;
+  std::vector<std::string> _tags;
+
+  std::vector<BaseBar*> _bars;
+
+  // BaseSheetProperty * _property;
+
  public:
   BaseSheet();
   BaseSheet(BaseSheet& ivRh);
+
+  virtual void setTitle(const std::string& ivTitle);
+  virtual void setSubtitle(const std::string& ivTitle);
+  virtual void setSubsubTitle(const std::string& ivTitle);
+  virtual void setComposer(const std::string& ivName);
+  virtual void setOpus(const std::string& ivOpusSystem, const UInt32 ivOpusNumber);
+
+  virtual std::string getTitle() const;
+  virtual std::string getSubtitle() const;
+  virtual std::string getSubsubtitle() const;
+  virtual std::string getComposer() const;
+  virtual std::string getOpus() const;
+  virtual std::string getOpusSystem() const;
+  virtual UInt32 getOpusNumber() const;
+
+  virtual BaseBar * getBarNumber(const UInt32 ivBarNbr) const;
+  virtual void appendBar(BaseBar * ivNewBar);
+  virtual UInt32 countBars() const;
 };
 
 } // namespace core
