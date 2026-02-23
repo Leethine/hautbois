20288b8b531ac002938963a084a2e6bdf4b2c474
Z L <zian.li.1996@outlook.com>
2026-02-22 22:47:04 -0600

    WIP voice

---
+++ symbol/voice.cpp
@@ -0,0 +1,292 @@
+#include "voice.hpp"
+#include "note.hpp"
+#include "duration.hpp"
+
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+namespace hautbois {
+namespace core {
+
+void Voice::logDurationPtr(Duration * __meter) {
+  if (__meter) {
+    _durationPtrLog.push_back(__meter);
+  }
+}
+
+void Voice::logNotePtr(Note * __note) {
+  if (__note) {
+    _notePtrLog.push_back(__note);
+  }
+}
+
+Voice::Voice() : 
+_currentBar (0), _currentTV (0), _newBarPos ({0}),
+_meter (nullptr), _meterDefault (nullptr),
+_meterList ({}), _noteList ({}), _durationPtrLog ({}),
+_notePtrLog ({}), _temporaryVoiceList ({})
+{
+  Duration * d = new Duration(4,4);
+  logDurationPtr(d);
+  _meter = d;
+  _meterDefault = d;
+}
+
+Voice::Voice(const int& __num, const int& __denom): Voice() {
+  Duration * d = new Duration(__num, __denom);
+  logDurationPtr(d);
+  _meter = d;
+  _meterDefault = d;
+}
+
+Voice::~Voice() {
+  for (auto it = _durationPtrLog.begin(); it != _durationPtrLog.end(); it++) {
+    delete (*it);
+  }
+  for (auto it = _notePtrLog.begin(); it != _notePtrLog.end(); it++) {
+    delete (*it);
+  }
+}
+
+const Duration * Voice::getMeter() const {
+  if (_meter) {
+    return _meter;
+  }
+  else {
+    return _meterDefault;
+  }
+}
+
+const Duration * Voice::getMeter(const int& __bar) const {
+  if (_meterList.empty()) {
+    return _meterDefault;
+  }
+  else if (__bar < _meterList.size()) {
+    return _meterList[__bar];
+  }
+  else {
+    return _meterList.back();
+  }
+}
+
+const Note * Voice::getNote(const int& __bar, const int& __nth) const {
+  if (__bar < _newBarPos.size()) {
+    int idx = _newBarPos[__bar] + __nth;
+    if (idx < _noteList.size()) {
+      return _noteList[idx];
+    }
+    else {
+      throw std::out_of_range(" Note no. " + std::to_string(__nth));
+    }
+  }
+  else {
+    throw std::out_of_range("Bar no. "  + std::to_string(__bar));
+  }
+}
+
+const Note * Voice::getNoteTV(const int& __bar,
+                              const int& __nth,
+                              int __voice) const {
+  for (const TemporaryVoice& tv : _temporaryVoiceList) {
+    if (__bar == tv._bar && __voice == tv._voice) {
+      if (__nth < tv._noteList.size()) {
+        return tv._noteList[__nth];
+      }
+    }
+  }
+  throw std::out_of_range("Bar no. "    + std::to_string(__bar) +
+                          " Voice no. " + std::to_string(__voice) +
+                          " Note no. "  + std::to_string(__nth));
+}
+
+void * Voice::toStream(const std::string& __instr, void * __ostream) const {
+  // not supported
+  return nullptr;
+}
+
+bool Voice::barHasTempVoice(const int& __bar) const {
+  for (const TemporaryVoice& tv : _temporaryVoiceList) {
+    if (__bar == tv._bar) {
+      return true;
+    }
+  }
+  return false;
+}
+
+int Voice::getNbrOfTempVoice(const int& __bar) const {
+  int count = 0;
+  for (const TemporaryVoice& tv : _temporaryVoiceList) {
+    if (__bar == tv._bar) {
+      count++;
+    }
+  }
+  return count;
+}
+
+bool Voice::barCheckMainVoiceThrowExp() const {
+  //TODO
+  return true;
+}
+
+bool Voice::barCheckTempVoiceThrowExp() const {
+  //TODO
+  return true;
+}
+
+bool Voice::barCheck() const {
+  return barCheckMainVoiceThrowExp() &&
+         barCheckTempVoiceThrowExp();
+}
+
+void Voice::copyTo(Voice * __ivoice) const {
+  //TODO
+}
+
+int Voice::getCurrentBar() const {
+  return _currentBar;
+}
+
+void Voice::setCurrentBar(const int& __bar) {
+  _currentBar = __bar;
+}
+
+void Voice::setCurrentTV(const int& __voice) {
+  _currentTV = __voice;
+}
+
+void Voice::resetNthNote(const int& __bar, const int& __nth, Note * __note) {
+  if (__bar < _newBarPos.size()) {
+    int idx = _newBarPos[__bar] + __nth;
+    if (idx < _noteList.size()) {
+      if (__note) {
+        _noteList[idx] = __note;
+        logNotePtr(__note);
+      }
+      else {
+        throw std::runtime_error("input was nullptr");
+      }
+    }
+    else {
+      throw std::out_of_range(" Note no. " + std::to_string(__nth));
+    }
+  }
+  else {
+    throw std::out_of_range("Bar no. "  + std::to_string(__bar));
+  }
+}
+
+void Voice::resetNthNoteTV(const int& __bar, const int& __nth, const int& __voice, Note * __note) {
+  for (TemporaryVoice& tv : _temporaryVoiceList) {
+    if (__bar == tv._bar && __voice == tv._voice) {
+      if (__nth < tv._noteList.size()) {
+        if (__note) {
+          tv._noteList[__nth] = __note;
+          logNotePtr(__note);
+          return ;
+        }
+        else {
+          throw std::runtime_error("input was nullptr");
+        }
+      }
+      else {
+        throw std::out_of_range("Note no. "  + std::to_string(__nth));
+      }
+    }
+  }
+  throw std::out_of_range("Bar no. "    + std::to_string(__bar) +
+                          " Voice no. " + std::to_string(__voice));
+}
+
+void Voice::addNote(const int& __num, const int& __denom,
+             const std::string& __pitch) {
+  Note * n = new Note(__num, __denom, __pitch);
+  _noteList.push_back(n);
+  logNotePtr(n);
+}
+
+void Voice::addNote(Note * __note) {
+  if (__note) {
+    _noteList.push_back(__note);
+    logNotePtr(__note);
+  }
+  else {
+    throw std::runtime_error("input was nullptr");
+  }
+}
+
+void Voice::newBar(const int& __num, const int& __denom) {
+  if (__num == _meter->getNum() && __denom == _meter->getDenom()) {
+    _meterList.push_back(_meter);
+  }
+  else {
+    Duration * d = new Duration(__num, __denom);
+    logDurationPtr(d);
+  }
+  _currentBar++;
+  _currentTV = 0;
+  _newBarPos.push_back(_noteList.size()+1);
+}
+
+void Voice::newBar() {
+  _meterList.push_back(_meter);
+  _currentBar++;
+  _currentTV = 0;
+  _newBarPos.push_back(_noteList.size()+1);
+}
+
+void Voice::newTempVoice(const int& __bar) {
+  if (__bar < _newBarPos.size()) {
+    TemporaryVoice v;
+    v._bar = __bar;
+    v._voice = _currentTV++;
+    _temporaryVoiceList.push_back(v);
+  }
+  else {
+    throw std::out_of_range("Bar no. "  + std::to_string(__bar));
+  }
+}
+
+void Voice::newTempVoice() {
+  newTempVoice(_currentBar);
+}
+
+void Voice::addNoteTV(const int& __num, const int& __denom,
+                      const std::string& __pitch) {
+  for (TemporaryVoice& tv : _temporaryVoiceList) {
+    if (tv._bar == _currentBar && tv._voice == _currentTV) {
+      Note * n = new Note(__num, __denom, __pitch);
+      logNotePtr(n);
+      tv._noteList.push_back(n);
+      return ;
+    }
+    else {
+      throw std::out_of_range("Bar no. "    + std::to_string(_currentBar) +
+                              " Voice no. " + std::to_string(_currentTV));
+    }
+  }
+  throw std::out_of_range("Missing temporary voice");
+}
+
+void Voice::addNoteTV(Note * __note) {
+  for (TemporaryVoice& tv : _temporaryVoiceList) {
+    if (tv._bar == _currentBar && tv._voice == _currentTV) {
+      if (__note) {
+        logNotePtr(__note);
+        tv._noteList.push_back(__note);
+        return ;
+      }
+      else {
+        std::runtime_error("input was nullptr");
+      }
+    }
+    else {
+      throw std::out_of_range("Bar no. "    + std::to_string(_currentBar) +
+                              " Voice no. " + std::to_string(_currentTV));
+    }
+  }
+  throw std::out_of_range("Missing temporary voice");
+}
+
+} // namespace core
+} // namespace hautbois
--- symbol/voice.hpp
+++ symbol/voice.hpp
@@ -16,6 +16,8 @@ struct TemporaryVoice {
 
   int _bar;
 
+  int _voice;
+
   std::vector<Note *> _noteList;
 
 };
@@ -24,21 +26,29 @@ class Voice {
 
  protected:
 
+  int _currentBar;
+
+  int _currentTV;
+
+  std::vector<int> _newBarPos;
+
   Duration * _meter;
 
-  int _currentBar = 0;
-
-  int _currentTV = 0;
+  Duration * _meterDefault;
 
   std::vector<Duration *> _meterList;
 
   std::vector<Note *> _noteList;
 
-  std::vector<TemporaryVoice> _temporaryVoiceList;
-
   std::vector<Duration *> _durationPtrLog;
 
   std::vector<Note *> _notePtrLog;
+  
+  std::vector<TemporaryVoice> _temporaryVoiceList;
+
+  virtual void logDurationPtr(Duration * __meter);
+
+  virtual void logNotePtr(Note * __note);
 
   Voice(const Voice&)=delete;
 
@@ -52,19 +62,21 @@ class Voice {
 
   Voice();
 
-  Voice(int iNum, int iDenom);
+  Voice(const int& __num, const int& __denom);
 
   virtual ~Voice();
 
   virtual const Duration * getMeter() const;
 
-  virtual const Duration * getMeterAtBar(const int& __bar) const;
+  virtual const Duration * getMeter(const int& __bar) const;
 
   virtual const Note * getNote(const int& __bar, const int& __nth) const;
 
   virtual const Note * getNoteTV(const int& __bar, const int& __nth, int __voice=0) const;
 
-  virtual void barHasTempVoice(const int& __bar) const;
+  virtual void * toStream(const std::string& __instr, void * __ostream) const;
+
+  virtual bool barHasTempVoice(const int& __bar) const;
 
   virtual int getNbrOfTempVoice(const int& __bar) const;
 
@@ -76,20 +88,29 @@ class Voice {
 
   virtual void copyTo(Voice * __ivoice) const;
 
+  virtual int getCurrentBar() const;
+
+  virtual void setCurrentBar(const int& __bar);
+
+  virtual void setCurrentTV(const int& __voice=0);
+
   virtual void resetNthNote(const int& __bar, const int& __nth, Note * __note);
 
   virtual void resetNthNoteTV(const int& __bar, const int& __nth, const int& __voice, Note * __note);
 
-  virtual void newBar(int __num, int __denom);
-
-  virtual void newBar();
 
   virtual void addNote(const int& __num, const int& __denom,
                        const std::string& __pitch);
 
   virtual void addNote(Note * __note);
 
+  virtual void newBar(const int& __num, const int& __denom);
+
+  virtual void newBar();
+
   virtual void newTempVoice(const int& __bar);
+
+  virtual void newTempVoice();
 
   virtual void addNoteTV(const int& __num, const int& __denom,
                          const std::string& __pitch);
