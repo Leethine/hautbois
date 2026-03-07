f8dd3f8a6c2576fc58eacd41ee566cbff5122a0e
Z L <zian.li.1996@outlook.com>
2026-03-07 16:41:28 -0600

    WIP lilypond note

--- adapter/lilypond/italiano/note.cpp
+++ adapter/lilypond/italiano/note.cpp
@@ -1,7 +1,18 @@
 #include "note.hpp"
+#include <regex>
 
 namespace hautbois {
 namespace ly {
+
+
+const static std::regex SINGLE_NOTE_REGEX
+  ("^[do|re|mi|fa|sol|la|si]{1}[\'|,]*[1|2|4|8|16|32|64|128]{1}(.)*$");
+
+const static std::regex CHORD_REGEX
+  ("^<([do|re|mi|fa|sol|la|si][\'|,]*[::blank::]?)+>[1|2|4|8|16|32|64|128]{1}(.)*$");
+
+const static std::regex TUPLE_REGEX ("TODO");
+//TODO
 
 void LyNoteIt::setNoteType(core::NoteType __ntype) {
   core::Note::setNoteType(__ntype);
@@ -25,6 +36,37 @@ void LyNoteIt::setDuration(core::Duration * __d) {
 
 void LyNoteIt::setProperty(core::Property * __p) {
   core::Note::setProperty(__p);
+}
+
+
+core::NoteType LyNoteIt::guessNoteType(const std::string& __input) const {
+  if (__input == "r" || __input == "R") {
+    return core::NoteType::Rest;
+  }
+  else if (__input == "s" || __input == "S") {
+    return core::NoteType::Rest;
+  }
+  else if (__input.size() > 5 && __input.substr(__input.size() - 5) == "\\rest") {
+    return core::NoteType::Rest;
+  }
+  else if (__input.find('<') != std::string::npos &&
+      __input.find('>') != std::string::npos) {
+    return core::NoteType::Chord;
+  }
+  else if (__input.find('{') != std::string::npos &&
+           __input.find('}') != std::string::npos) {
+    return core::NoteType::Tuplet;
+  }
+  else {
+    return core::NoteType::SingleNote;
+  }
+}
+
+bool LyNoteIt::checkFormatThrowExp(const std::string& __pitch) const {
+  if (getType() == core::NoteType::SingleNote) {
+    // TODO
+  }
+  // TODO
 }
 
 } // namespace hautbois
