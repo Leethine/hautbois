f5d9b0932cf3e5c9f99f0555b7f3cca912d6c0a5
Z L <zian.li.1996@outlook.com>
2026-03-15 15:25:31 -0500

    Create lilypond pitch (italiano)

---
+++ adapter/lilypond/ly_pitch.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "symbol/pitch.hpp"
+
+#ifndef LY_PITCH_H
+#define LY_PITCH_H
+
+#include <string>
+#include <vector>
+
+namespace hautbois {
+namespace ly {
+
+class LyPitch : public core::Pitch {
+
+ public:
+
+  LyPitch();
+
+  LyPitch(const std::string& __pitch);
+
+  LyPitch(const std::string& __name, const int& __octave);
+
+  LyPitch(const Pitch& p);
+
+  LyPitch(const Pitch&& p);
+
+  LyPitch(const LyPitch& p);
+
+  LyPitch(const LyPitch&& p);
+
+  virtual ~LyPitch();
+
+  LyPitch& operator=(const LyPitch& p);
+
+  LyPitch& operator=(const core::Pitch& p);
+
+  virtual std::string getName() const;
+
+  virtual std::string getAccidental() const;
+  
+  virtual std::string getOctave() const;
+
+  virtual int getOctaveInt() const;
+
+  virtual const core::PitchRaw& raw() const;
+  
+  virtual core::PitchRaw getRaw() const;
+
+  virtual std::string toString() const;
+
+  virtual int toIndex() const;
+
+  virtual double toFrequency(const std::string& __temperament) const;
+
+  virtual void modify(const char * __context);
+
+  virtual bool operator==(const LyPitch& p) const;
+
+  virtual bool operator!=(const LyPitch& p) const;
+
+  virtual bool operator>(const LyPitch& p) const;
+
+  virtual bool operator<(const LyPitch& p) const;
+
+  virtual bool operator>=(const LyPitch& p) const;
+
+  virtual bool operator<=(const LyPitch& p) const;
+
+};
+
+} // namespace hautbois
+} // namespace core
+
+#endif
