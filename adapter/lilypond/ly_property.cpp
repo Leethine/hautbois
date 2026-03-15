54752aa27e3838a4ca7cd2d73a627b3b81baf0d8
Z L <zian.li.1996@outlook.com>
2026-03-15 18:50:04 -0500

    Create Lilypond property

---
+++ adapter/lilypond/ly_property.cpp
@@ -0,0 +1,106 @@
+#include "ly_property.hpp"
+
+#include <algorithm>
+#include <cctype>
+#include <string>
+#include <vector>
+#include <array>
+
+namespace hautbois {
+namespace ly {
+
+const static std::array<std::string, 39> LY_NOTE_PROPERTY {
+  // dynamic marks
+  "<",">","!","p","pp","ppp","f","ff","fff","mp","mf","fp","pf","cresc","decresc",
+  // articulation marks
+  "accent", "espressivo", "marcato", "portato", "staccatissimo", "staccato", "tenuto",
+  "fermata",
+  // Ornament 
+  "prall", "prallup", "pralldown", "upprall", "downprall", "prallprall",
+  "lineprall", "prallmordent", "mordent", "upmordent", "downmordent",
+  "trill", "turn", "reverseturn", "slashturn", "haydnturn"
+};
+
+LyProperty::LyProperty() : core::Property() { }
+
+// LyProperty::LyProperty(const std::string& text) :
+//  core::Property(text) { }
+
+LyProperty::LyProperty(const core::Property& p) :
+  core::Property(p.raw().getStr()) { }
+  
+LyProperty::LyProperty(const core::Property&& p) :
+  core::Property(p.raw().getStr()) { }
+
+LyProperty::LyProperty(const LyProperty& p) :
+  core::Property(p.raw().getStr()) { }
+  
+LyProperty::LyProperty(const LyProperty&& p) :
+  core::Property(p.raw().getStr()) { }
+  
+LyProperty::~LyProperty() {
+}
+  
+LyProperty& LyProperty::operator=(const LyProperty& p) {
+  if (this != &p) {
+    _raw.set(p.raw().getStr());
+  }
+  return *this;
+}
+
+void LyProperty::modify(const char * context) {
+  if (context == nullptr) {
+    return ;
+  }
+  std::vector<std::string> propertyList;
+  for (int i = 0; i<std::strlen(context); i++) {
+    std::string s;
+    if (context[i] == '\\') {
+      int j=i+1;
+      for (; context[j] != '\\' && context[j] != '\0'; j++, i++) {
+        s.push_back(context[j]);
+      }
+      i = j;
+    }
+    if (!s.empty()) {
+      propertyList.push_back(s);
+    }
+  }
+  std::string newtext;
+  for (std::string& s: propertyList) {
+    if (std::find(LY_NOTE_PROPERTY.begin(), LY_NOTE_PROPERTY.end(), s) 
+        != LY_NOTE_PROPERTY.end()) {
+        newtext.push_back('\\');
+        newtext.append(s);
+    }
+  }
+  _raw.set(newtext);
+}
+
+void * LyProperty::convert(const char* context) {
+  // TODO
+  return nullptr;
+}
+
+bool LyProperty::hasValue() const {
+  return core::Property::hasValue();
+}
+
+std::string LyProperty::toString() const {
+  return _raw.getStr();
+}
+
+bool LyProperty::operator==(const std::string& s) const {
+  return _raw.getStr() == s;
+}
+
+const core::PropertyRaw& LyProperty::raw() const {
+  return core::Property::raw();
+}
+
+core::PropertyRaw LyProperty::getRaw() const {
+  return core::Property::getRaw();
+}
+
+} // namespace ly
+} // namespace hautbois
