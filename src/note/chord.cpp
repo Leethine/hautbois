84a0e0a24010164e801ea90c27676e745e9d888b
ZL <zli@cpp.dev>
2026-07-19 21:18:29 -0500

    Reimplement chord note

---
+++ src/note/chord.cpp
@@ -0,0 +1,176 @@
+#include "chord.hpp"
+#include "../hbtype/hbdefs.hpp"
+#include "../utility/hbexcept.hpp"
+#include "note.hpp"
+#include <stdexcept>
+#include <utility>
+
+namespace hautbois {
+
+void Chord::setNoteType(const char __note_type) {
+  if (__note_type == CHAR_NOTETYPE_CHORD) {
+    Note::setNoteType(CHAR_NOTETYPE_CHORD);
+  }
+}
+
+Chord::Chord(const std::vector<std::string>& __pitch, const std::string& __value) :
+  Note(CHAR_NOTETYPE_CHORD) {
+
+  Duration * d_ptr = nullptr;
+  HB_NESTED_THROW_MSG(std::invalid_argument,
+    std::string("Failed to create chord") ,
+    d_ptr = new Duration(__value);
+  )
+  Chord::setDuration(d_ptr, NOTE_SET_METHOD_APPEND_POS);
+
+  std::vector<Pitch *> temp_pitch;
+  for (auto& str: __pitch) {
+    HB_NESTED_THROW_MSG_ACTION(std::invalid_argument,
+      std::string("Failed to create chord.") ,
+      temp_pitch.push_back(new Pitch(str)); ,
+      for (Pitch * p : temp_pitch) {
+        delete p;
+      }
+    )
+  }
+  // Call addPitch method
+  for (Pitch * p : temp_pitch) {
+    Chord::setPitch(p, NOTE_SET_METHOD_APPEND_POS);
+  }
+}
+
+Chord::Chord(const Chord& __other) : Chord(std::forward<const Chord&&>(__other)) {
+}
+
+Chord::Chord(const Chord&& __other) : Note(CHAR_NOTETYPE_CHORD) {
+  // Process note value
+  Duration * d_ptr = nullptr;
+  if (Chord::getDuration()) {
+    HB_NESTED_THROW_MSG(std::invalid_argument,
+      std::string("Failed to copy construct chord") ,
+      d_ptr = new Duration(Chord::getDuration()->toString());
+    )
+    Chord::setDuration(d_ptr, NOTE_SET_METHOD_APPEND_POS);
+  }
+
+  // process pitch
+  std::vector<Pitch *> temp_pitch;
+  for (int i = 0; i < Chord::getSize(); i++) {
+    if (Chord::getPitch(i)) {
+      HB_NESTED_THROW_MSG_ACTION(std::invalid_argument,
+        std::string("Failed to copy construct chord.") ,
+        temp_pitch.push_back(new Pitch(Chord::getPitch(i)->toString())); ,
+        for (Pitch * p : temp_pitch) {
+          delete p;
+        }
+      )
+    }
+    else {
+      HB_THROW_MSG(std::runtime_error,
+        std::string("Run time error occurred while calling chord copy constructor."));
+    }
+  }
+  // Call addPitch method
+  for (Pitch * p : temp_pitch) {
+    Chord::setPitch(p, NOTE_SET_METHOD_APPEND_POS);
+  }
+  // process ties
+  for (int i = 0; i < Chord::getSize(); i++) {
+    if (Chord::isTied(i)) {
+      Chord::makeTie(i);
+    }
+  }
+
+  // Process property
+  for (int i = 0; i < Chord::getSize(); i++) {
+    if (Chord::getProperty(i)) {
+      Chord::setProperty(new Property(Chord::getProperty(i)->toString()), NOTE_SET_METHOD_APPEND_POS);
+    }
+    else {
+      Chord::setProperty(nullptr, NOTE_SET_METHOD_APPEND_POS);
+    }
+  }
+}
+
+void Chord::addProperty(const std::string& __property, const int __pos) {
+  Property * ptr = new Property(__property);
+  Chord::setProperty(ptr, __pos);
+}
+
+int Chord::getSize() const {
+  int count = 0;
+  while(Chord::getPitch(count++)) {
+  }
+  return count;
+}
+
+bool Chord::isValid() const {
+  if (Chord::getDuration(0) == nullptr) {
+    return false;
+  }
+  int s = Chord::getSize();
+  for (int i = 0; i < s; i++) {
+    if (Chord::getPitch(i) == nullptr) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void Chord::transpose(const int __degree, const std::string& __tonality, const std::string& __mode) {
+  int s = Chord::getSize();
+  for (int i = 0; i < s; i++) {
+    if (Chord::getPitchModify(i)) {
+      Chord::getPitchModify(i)->transpose(__degree, __tonality, __mode);
+    }
+  }
+}
+
+void Chord::enlarge(const int __factor) {
+  if (Chord::getDurationModify(0)) {
+    Chord::getDurationModify(0)->multiply(__factor);
+  }
+}
+
+void Chord::reduce(const int __factor) {
+  if (Chord::getDurationModify(0)) {
+    Chord::getDurationModify(0)->divide(__factor);
+  }
+}
+
+std::string Chord::toString() const {
+  std::string out;
+  int s = Chord::getSize();
+  for (int i = 0; i < s; i++) {
+    if (Chord::getPitch(i)) {
+      out.append(Chord::getPitch(i)->toString());
+    }
+    else {
+      out.push_back('?');
+    }
+    out.push_back('+');
+  }
+  if (!out.empty() && out.back() == '+') {
+    out.pop_back();
+    out.push_back(',');
+  }
+  if (Chord::getDuration(0)) {
+    out.append(Chord::getDuration(0)->toString());
+  }
+  else {
+    out.push_back('?');
+  }
+  return out;
+}
+
+void * Chord::toStream(void * __output, void * __param) const {
+  // TODO
+  return nullptr;
+}
+
+void * Chord::serialize(const int __version, void * __param) const {
+  // TODO
+  return nullptr;
+}
+
+} // namespace hautbois
---
+++ src/note/chord.hpp
@@ -0,0 +1,117 @@
+#pragma once
+
+#ifndef CHORD_HPP
+#define CHORD_HPP
+
+#include "note.hpp"
+
+namespace hautbois {
+
+class Chord : public Note {
+
+protected:
+
+  inline virtual void setNote(Note * __ptr, const int __pos) {
+    Note::setNote(__ptr, __pos);
+  }
+
+  inline virtual void setPitch(Pitch * __ptr, const int __pos) {
+    Note::setPitch(__ptr, __pos);
+  }
+
+  inline virtual void setDuration(Duration * __ptr, const int __pos) {
+    Note::setDuration(__ptr, __pos);
+  }
+
+  inline virtual void setProperty(Property * __ptr, const int __pos) {
+    Note::setProperty(__ptr, __pos);
+  }
+
+  inline virtual Pitch * getPitchModify(const size_t __pos) {
+    return Note::getPitchModify(__pos);
+  }
+
+  inline virtual Duration * getDurationModify(const size_t __pos) {
+    return Note::getDurationModify(__pos);
+  }
+
+  inline virtual Property * getPropertyModify(const size_t __pos) {
+    return Note::getPropertyModify(__pos);
+  }
+
+  inline virtual Note * getNoteModify(const size_t __pos) {
+    return Note::getNoteModify(__pos);
+  }
+
+  virtual void setNoteType(const char __note_type);
+
+public:
+
+  inline virtual ~Chord() {}
+
+  inline virtual bool isType(const char __note_type) const {
+    return Note::isType(__note_type);
+  }
+
+  inline virtual char getType() const {
+    return Note::getType();
+  }
+
+  inline virtual void makeTie(const size_t __pos = 0) {
+    Note::makeTie(__pos);
+  }
+
+  inline virtual void makeUntie(const size_t __pos = 0) {
+    Note::makeUntie(__pos);
+  }
+
+  inline virtual bool isTied(const size_t __pos = 0) const {
+    return Note::isTied(__pos);
+  }
+
+  inline virtual const Pitch * getPitch(const size_t __pos = 0) const {
+    return Note::getPitch(__pos);
+  }
+
+  inline virtual const Duration * getDuration(const size_t __pos = 0) const {
+    return Note::getDuration(__pos);
+  }
+
+  inline virtual const Property * getProperty(const size_t __pos = 0) const {
+    return Note::getProperty(__pos);
+  }
+
+  inline virtual const Note * getNote(const size_t __pos = 0) const {
+    return nullptr;
+  }
+
+  Chord(const std::vector<std::string>& __pitch, const std::string& __value);
+
+  Chord(const Chord& __other);
+
+  Chord(const Chord&& __other);
+
+  // was declared abstract
+  virtual void addProperty(const std::string& __property, const int __pos = 0);
+
+  virtual int getSize() const;
+
+  virtual bool isValid() const;
+
+  virtual void transpose(const int __degree, const std::string& __tonality, const std::string& __mode);
+
+  virtual void enlarge(const int __factor);
+
+  virtual void reduce(const int __factor);
+
+  virtual std::string toString() const;
+
+  virtual void * toStream(void * __output, void * __param) const;
+
+  virtual void * serialize(const int __version, void * __param) const;
+
+};
+
+} // namespace hautbois
+
+#endif
