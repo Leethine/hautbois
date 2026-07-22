1fa4d307fe6576bf48996d9e87fae95866855867
ZL <zli@cpp.dev>
2026-07-21 22:45:33 -0500

    Create headers (to be implemented)

---
+++ src/theory/transpose.hpp
@@ -0,0 +1,17 @@
+
+namespace hautbois {
+namespace theory {
+
+enum class Intervals {
+  unisson = 0,
+  seconde = 1,
+  tierce  = 2,
+  quarte  = 3,
+  quinte  = 4,
+  sixte   = 5,
+  septieme= 6,
+  octave  = 7
+};
+
+} // namespace theory
+} // namespace hautbois
