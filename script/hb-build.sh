8a389a6330ca949b435744567bd052b4f6086388
ZL <zli@cpp.dev>
2026-08-21 10:46:14 -0500

    Improve build script

--- script/hb-build.sh
+++ script/hb-build.sh
@@ -6,7 +6,15 @@ if [[ "${1}" == "--help" ]] || [[ "${1}" == "-h" ]]; then
 fi
 
 src_file="${1}"
+if [[ -z "${src_file}" ]]; then
+  src_file="__PREPROCESSED__.cpp"
+fi
+
 target_file="${2}"
+if [[ -z "${target_file}" ]]; then
+  target_file="__BIN__"
+fi
+
 linked_lib_path=""
 
 ###__TEXT_REPLACE_TAG__###
@@ -20,10 +28,6 @@ fi
 if [[ -d "${target_file}" ]]; then
   echo "Cannot override dest file: ${target_file}"
   exit 1
-fi
-
-if [[ -z "${target_file}" ]]; then
-  target_file="$(echo "${src_file}" | cut -d '.' -f1 | tr ' ' '_')"
 fi
 
 g++ ${linked_lib_path} -std=c++17 "${src_file}" -lm -lasound -lstk -lHb -o "${target_file}" && ./${target_file}
